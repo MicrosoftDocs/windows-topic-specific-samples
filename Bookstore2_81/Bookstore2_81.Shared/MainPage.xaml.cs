e35b225abbf9ae33babe114ac4467785faf6e304
Karl Erickson <karler@microsoft.com>
2017-03-23 15:30:43 -0700

    added sample

---
+++ Bookstore2_81/Bookstore2_81.Shared/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿/* 
+    Copyright (c) Microsoft Corporation. All rights reserved.
+    Use of this sample source code is subject to the terms of the Microsoft license 
+    agreement under which you licensed this sample source code and is provided AS-IS.
+    If you did not accept the terms of the license agreement, you are not authorized 
+    to use this sample source code.  For the terms of the license, please see the 
+    license agreement between you and Microsoft.
+  
+    To see all code Samples for Windows Store apps and Windows Phone Store apps, visit http://code.msdn.microsoft.com/windowsapps
+  
+*/
+
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Bookstore2_81
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
