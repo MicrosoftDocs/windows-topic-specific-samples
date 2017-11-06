ef2989810ca52e9379d055f5acff8d1ce27d4a16
Karl Bridge <kbridge@microsoft.com>
2017-11-06 14:52:27 -0800

    checklist cleanup

---
+++ README.md
@@ -0,0 +1,22 @@
+---
+category: CustomUserInteractions Inking
+---
+
+# Ink toolbar location and orientation sample (basic)
+
+> The default platform target is ARM, change this to x64 or x86 if you want to test on a non-ARM device.
+
+This sample supports the [Pen interactions and Windows Ink](https://docs.microsoft.com/en-us/windows/uwp/input-and-devices/pen-and-stylus-interactions) topics on the [Windows Dev Center](https://developer.microsoft.com/en-us/windows).
+
+In this sample, we demonstrate how to set the location and orientation of the ink toolbar in a basic UWP application using the [Windows.UI.Xaml.Controls.InkToolbar](https://docs.microsoft.com/uwp/api/windows.ui.xaml.controls.inktoolbar) and [Windows.UI.ViewManagement.UISettings](https://docs.microsoft.com/uwp/api/windows.ui.viewmanagement.uisettings) APIs to get user preference and device state. 
+
+We focus on the following:
+* Adding basic ink support
+* Adding an InkToolbar
+* Setting the location and orientation of the ink toolbar based on the left or right-hand writing preferences specified through **Settings > Devices > Pen & Windows Ink > Pen > Choose which hand you write with**
+
+## Contributing
+
+We welcome your input on issues and suggestions for new samples! Please file them as issues on this GitHub repo.  At this time we are not accepting new samples from the public, but please check back as we evolve our contribution model.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
---
+++ location-and-orientation/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace locationandorientation
+{
+    /// <summary>
+    /// Our application's single UI page.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Windows.UI.ViewManagement.UISettings settings = 
+                new Windows.UI.ViewManagement.UISettings();
+            HorizontalAlignment alignment = 
+                (settings.HandPreference == 
+                    Windows.UI.ViewManagement.HandPreference.LeftHanded) ? 
+                    HorizontalAlignment.Left : HorizontalAlignment.Right;
+            inkToolbar.HorizontalAlignment = alignment;
+        }
+    }
+}
