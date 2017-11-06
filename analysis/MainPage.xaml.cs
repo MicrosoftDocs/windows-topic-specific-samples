79d6213c4ac4d6a020d0d9c492579edef27552b5
Karl Bridge <kbridge@microsoft.com>
2017-11-06 15:16:48 -0800

    Initial commit

---
+++ analysis/MainPage.xaml.cs
@@ -0,0 +1,227 @@
+﻿//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+using Windows.UI.Input.Inking;
+using Windows.UI.Input.Inking.Analysis;
+using Windows.UI.Xaml.Shapes;
+
+namespace Analysis 
+{
+    /// <summary>
+    /// Our application's single UI page.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Stroke recognition globals
+        InkAnalyzer inkAnalyzer = new InkAnalyzer();
+        IReadOnlyList<InkStroke> inkStrokes = null;
+        InkAnalysisResult inkAnalysisResults = null;
+
+        /// <summary>
+        /// Initialize the UI page.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Set supported inking device types.
+            inkCanvas.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen |
+                Windows.UI.Core.CoreInputDeviceTypes.Touch;
+
+            // Set initial ink stroke attributes.
+            InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            drawingAttributes.Color = Windows.UI.Colors.Black;
+            drawingAttributes.IgnorePressure = false;
+            drawingAttributes.FitToCurve = true;
+            inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+
+            // Listen for button click to initiate recognition.
+            analyze.Click += AnalyzeStrokes_Click;
+        }
+
+        /// <summary>
+        /// The "Analyze" button click handler.
+        /// Ink recognition is performed here.
+        /// </summary>
+        /// <param name="sender">Source of the click event</param>
+        /// <param name="e">Event args for the button click routed event</param>
+        private async void AnalyzeStrokes_Click(object sender, RoutedEventArgs e)
+        {
+            inkStrokes = inkCanvas.InkPresenter.StrokeContainer.GetStrokes();
+            // Ensure an ink stroke is present.
+            if (inkStrokes.Count > 0)
+            {
+                inkAnalyzer.AddDataForStrokes(inkStrokes);
+
+                // If you're only interested in a specific type of recognition,
+                // such as writing or drawing, you can constrain recognition 
+                // using the SetStrokDataKind method as follows:
+                // foreach (var stroke in strokesText)
+                // {
+                //     analyzerText.SetStrokeDataKind(stroke.Id, InkAnalysisStrokeKind.Writing);
+                // }
+                // This can improve both efficiency and recognition results.
+                // In this example, we try to recognizing both, so the platform default 
+                // of "InkAnalysisStrokeKind.Auto" is used.
+                inkAnalysisResults = await inkAnalyzer.AnalyzeAsync();
+
+                // Have ink strokes on the canvas changed?
+                if (inkAnalysisResults.Status == InkAnalysisStatus.Updated)
+                {
+                    // Find all strokes that are recognized as handwriting and 
+                    // create a corresponding ink analysis InkWord node.
+                    var inkwordNodes =
+                        inkAnalyzer.AnalysisRoot.FindNodes(
+                            InkAnalysisNodeKind.InkWord);
+
+                    // Iterate through each InkWord node.
+                    // Draw primary recognized text on recognitionCanvas 
+                    // (for this example, we ignore alternatives), and delete 
+                    // ink analysis data and recognized strokes.
+                    foreach (InkAnalysisInkWord node in inkwordNodes)
+                    {
+                        // Draw a TextBlock object on the recognitionCanvas.
+                        DrawText(node.RecognizedText, node.BoundingRect);
+
+                        foreach (var strokeId in node.GetStrokeIds())
+                        {
+                            var stroke =
+                                inkCanvas.InkPresenter.StrokeContainer.GetStrokeById(strokeId);
+                            stroke.Selected = true;
+                        }
+                        inkAnalyzer.RemoveDataForStrokes(node.GetStrokeIds());
+                    }
+                    inkCanvas.InkPresenter.StrokeContainer.DeleteSelected();
+
+                    // Find all strokes that are recognized as a drawing and 
+                    // create a corresponding ink analysis InkDrawing node.
+                    var inkdrawingNodes =
+                        inkAnalyzer.AnalysisRoot.FindNodes(
+                            InkAnalysisNodeKind.InkDrawing);
+                    // Iterate through each InkDrawing node.
+                    // Draw recognized shapes on recognitionCanvas and
+                    // delete ink analysis data and recognized strokes.
+                    foreach (InkAnalysisInkDrawing node in inkdrawingNodes)
+                    {
+                        if (node.DrawingKind == InkAnalysisDrawingKind.Drawing)
+                        {
+                            // Catch and process unsupported shapes (lines and so on) here.
+                        }
+                        // Process generalized shapes here (ellipses and polygons).
+                        else
+                        {
+                            // Draw an Ellipse object on the recognitionCanvas 
+                            // (circle is a specialized ellipse).
+                            if (node.DrawingKind == InkAnalysisDrawingKind.Circle || 
+                                node.DrawingKind == InkAnalysisDrawingKind.Ellipse)
+                            {
+                                DrawEllipse(node);
+                            }
+                            // Draw a Polygon object on the recognitionCanvas.
+                            else
+                            {
+                                DrawPolygon(node);
+                            }
+                            foreach (var strokeId in node.GetStrokeIds())
+                            {
+                                var stroke = 
+                                    inkCanvas.InkPresenter.StrokeContainer.GetStrokeById(strokeId);
+                                stroke.Selected = true;
+                            }
+                        }
+                        inkAnalyzer.RemoveDataForStrokes(node.GetStrokeIds());
+                    }
+                    inkCanvas.InkPresenter.StrokeContainer.DeleteSelected();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draw ink recognition text string on the recognitionCanvas.
+        /// </summary>
+        /// <param name="recognizedText">The string returned by text recognition.</param>
+        /// <param name="boundingRect">The bounding rect of the original ink writing.</param>
+        private void DrawText(string recognizedText, Rect boundingRect)
+        {
+            TextBlock text = new TextBlock();
+            Canvas.SetTop(text, boundingRect.Top);
+            Canvas.SetLeft(text, boundingRect.Left);
+
+            text.Text = recognizedText;
+            text.FontSize = boundingRect.Height;
+
+            recognitionCanvas.Children.Add(text);
+        }
+
+        /// <summary>
+        /// Draw an ellipse on the recognitionCanvas.
+        /// </summary>
+        /// <param name="shape">The ink analysis node being processed.</param>
+        private void DrawEllipse(InkAnalysisInkDrawing shape)
+        {
+            var points = shape.Points;
+            Ellipse ellipse = new Ellipse();
+
+            ellipse.Width = shape.BoundingRect.Width;
+            ellipse.Height = shape.BoundingRect.Height;
+
+            Canvas.SetTop(ellipse, shape.BoundingRect.Top);
+            Canvas.SetLeft(ellipse, shape.BoundingRect.Left);
+
+            var brush = new SolidColorBrush(Windows.UI.ColorHelper.FromArgb(255, 0, 0, 255));
+            ellipse.Stroke = brush;
+            ellipse.StrokeThickness = 2;
+            recognitionCanvas.Children.Add(ellipse);
+        }
+
+        /// <summary>
+        /// Draw a polygon on the recognitionCanvas.
+        /// </summary>
+        /// <param name="shape">The ink analysis node being processed.</param>
+        private void DrawPolygon(InkAnalysisInkDrawing shape)
+        {
+            List<Point> points = new List<Point>(shape.Points);
+            Polygon polygon = new Polygon();
+
+            foreach (Point point in points)
+            {
+                polygon.Points.Add(point);
+            }
+
+            var brush = new SolidColorBrush(Windows.UI.ColorHelper.FromArgb(255, 0, 0, 255));
+            polygon.Stroke = brush;
+            polygon.StrokeThickness = 2;
+            recognitionCanvas.Children.Add(polygon);
+        }
+    }
+}
