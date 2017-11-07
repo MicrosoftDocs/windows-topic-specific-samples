1de1da72d9608c37b8ef216050bffde6e15f06d9
Karl Bridge <kbridge@microsoft.com>
2017-11-07 12:13:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<!--
+category: CustomUserInteractions Inking
+-->
+
+# Save and load ink strokes from an Ink Serialized Format (ISF) file
+
+> The default platform target is ARM, change this to x64 or x86 if you want to test on a non-ARM device.
+
+This sample supports the [Pen interactions and Windows Ink](https://docs.microsoft.com/en-us/windows/uwp/input-and-devices/pen-and-stylus-interactions) topics on the [Windows Dev Center](https://developer.microsoft.com/en-us/windows).
+
+In this sample, we use a basic UWP application to demonstrate how to serialize ink data and save it to an Ink Serialized Format (ISF) file. We also show how to load ink data from an ISF file and deserialize it for display on an InkCanvas control.  
+
+We focus on the following:
+* Adding basic ink support
+* Saving and loading ink stroke data to/from an ISF file
+
+## Contributing
+
+We welcome your input on issues and suggestions for new samples! Please file them as issues on this GitHub repo.  At this time we are not accepting new samples from the public, but please check back as we evolve our contribution model.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
---
+++ ink-store/MainPage.xaml.cs
@@ -0,0 +1,174 @@
+﻿//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using Windows.Storage.Streams;
+using Windows.UI.Input.Inking;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Ink_Store
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// Our application's single UI page.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Set supported inking device types.
+            inkCanvas.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen;
+
+            // Listen for button click to initiate save.
+            btnSave.Click += btnSave_Click;
+            // Listen for button click to initiate load.
+            btnLoad.Click += btnLoad_Click;
+            // Listen for button click to clear ink canvas.
+            btnClear.Click += btnClear_Click;
+        }
+
+        /// <summary>
+        /// Clear ink canvas of all ink strokes.
+        /// </summary>
+        /// <param name="sender">Source of the click event</param>
+        /// <param name="e">Event args for the button click routed event</param>
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.InkPresenter.StrokeContainer.Clear();
+        }
+
+
+        /// <summary>
+        /// Load ink data from a file, deserialize it, and add it to ink canvas.
+        /// </summary>
+        /// <param name="sender">Source of the click event</param>
+        /// <param name="e">Event args for the button click routed event</param>
+        private async void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            // Let users choose their ink file using a file picker.
+            // Initialize the picker.
+            Windows.Storage.Pickers.FileOpenPicker openPicker =
+                new Windows.Storage.Pickers.FileOpenPicker();
+            openPicker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add(".gif");
+            // Show the file picker.
+            Windows.Storage.StorageFile file = await openPicker.PickSingleFileAsync();
+            // User selects a file and picker returns a reference to the selected file.
+            if (file != null)
+            {
+                // Open a file stream for reading.
+                IRandomAccessStream stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                // Read from file.
+                using (var inputStream = stream.GetInputStreamAt(0))
+                {
+                    await inkCanvas.InkPresenter.StrokeContainer.LoadAsync(stream);
+                }
+                stream.Dispose();
+            }
+            // User selects Cancel and picker returns null.
+            else
+            {
+                // Operation cancelled.
+            }
+        }
+
+        /// <summary>
+        /// Get ink data from ink canvas, serialize it, and save it to a file.
+        /// </summary>
+        /// <param name="sender">Source of the click event</param>
+        /// <param name="e">Event args for the button click routed event</param>
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Get all strokes on the InkCanvas.
+            IReadOnlyList<InkStroke> currentStrokes = inkCanvas.InkPresenter.StrokeContainer.GetStrokes();
+
+            // Strokes present on ink canvas.
+            if (currentStrokes.Count > 0)
+            {
+                // Let users choose their ink file using a file picker.
+                // Initialize the picker.
+                Windows.Storage.Pickers.FileSavePicker savePicker =
+                    new Windows.Storage.Pickers.FileSavePicker();
+                savePicker.SuggestedStartLocation =
+                    Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+                savePicker.FileTypeChoices.Add(
+                    "GIF with embedded ISF",
+                    new List<string>() { ".gif" });
+                savePicker.DefaultFileExtension = ".gif";
+                savePicker.SuggestedFileName = "InkSample";
+
+                // Show the file picker.
+                Windows.Storage.StorageFile file =
+                    await savePicker.PickSaveFileAsync();
+                // When chosen, picker returns a reference to the selected file.
+                if (file != null)
+                {
+                    // Prevent updates to the file until updates are 
+                    // finalized with call to CompleteUpdatesAsync.
+                    Windows.Storage.CachedFileManager.DeferUpdates(file);
+                    // Open a file stream for writing.
+                    IRandomAccessStream stream = 
+                        await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+                    // Write the ink strokes to the output stream.
+                    using (IOutputStream outputStream = stream.GetOutputStreamAt(0))
+                    {
+                        await inkCanvas.InkPresenter.StrokeContainer.SaveAsync(outputStream);
+                        await outputStream.FlushAsync();
+                    }
+                    stream.Dispose();
+
+                    // Finalize write so other apps can update file.
+                    Windows.Storage.Provider.FileUpdateStatus status =
+                        await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+
+                    if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                    {
+                        // File saved.
+                    }
+                    else
+                    {
+                        // File couldn't be saved.
+                    }
+                }
+                // User selects Cancel and picker returns null.
+                else
+                {
+                    // Operation cancelled.
+                }
+            }
+        }
+    }
+}
