1b1ecb818d2d7490984b2a0fde4b04a127e6be16
Karl Bridge <kbridge@microsoft.com>
2019-03-11 13:03:35 -0700

    Initial commit

---
+++ XamlUICommand_Sample/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace XamlUICommand_Sample
+{
+    /// <summary>
+    /// A list item class with an associated Command and Command text.
+    /// </summary>
+    public class ListItemData
+    {
+        public String Text { get; set; }
+        public ICommand Command { get; set; }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// The collection of list items.
+        /// </summary>
+        ObservableCollection<ListItemData> collection = new ObservableCollection<ListItemData>();
+
+        private void ControlExample_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                collection.Add(new ListItemData { Text = "List item " + i.ToString(), Command = CustomXamlUICommand });
+            }
+        }
+
+        private void ListView_Loaded(object sender, RoutedEventArgs e)
+        {
+            var listView = (ListView)sender;
+            listView.ItemsSource = collection;
+        }
+
+        private void DeleteCommand_ExecuteRequested(XamlUICommand sender, ExecuteRequestedEventArgs args)
+        {
+            if (args.Parameter != null)
+            {
+                foreach (var i in collection)
+                {
+                    if (i.Text == (args.Parameter as string))
+                    {
+                        collection.Remove(i);
+                        return;
+                    }
+                }
+            }
+            if (ListViewRight.SelectedIndex != -1)
+            {
+                collection.RemoveAt(ListViewRight.SelectedIndex);
+            }
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListViewRight.SelectedIndex != -1)
+            {
+                var item = collection[ListViewRight.SelectedIndex];
+            }
+        }
+
+        private void ListViewSwipeContainer_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.Pointer.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse || e.Pointer.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Pen)
+            {
+                VisualStateManager.GoToState(sender as Control, "HoverButtonsShown", true);
+            }
+        }
+
+        private void ListViewSwipeContainer_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(sender as Control, "HoverButtonsHidden", true);
+        }
+    }
+}
