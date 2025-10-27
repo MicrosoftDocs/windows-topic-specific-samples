c360a5837d58dc221c3a6a3668bb4af4d4457c59
Alvin Ashcraft <aashcraft@microsoft.com>
2025-10-27 12:45:46 -0400

    Remove lines not needed

--- WinUINotes/WinUINotes/Views/AllNotesPage.xaml.cs
+++ WinUINotes/WinUINotes/Views/AllNotesPage.xaml.cs
@@ -6,9 +6,6 @@ using WinUINotes.ViewModels;
 
 namespace WinUINotes.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class AllNotesPage : Page
     {
         private AllNotesViewModel? viewModel;
--- WinUINotes/WinUINotes/Views/NotePage.xaml.cs
+++ WinUINotes/WinUINotes/Views/NotePage.xaml.cs
@@ -7,9 +7,6 @@ using WinUINotes.ViewModels;
 
 namespace WinUINotes.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class NotePage : Page
     {
         private NoteViewModel? noteVm;
